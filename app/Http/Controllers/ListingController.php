99fcd55de475c8cbe50c3b9cafdd7c70bdc59778
sagyndykovsan <sagyndykov.sanjar@gmail.com>
2022-11-03 10:05:09 +0600

    add listings/index,show

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('listings.index', [
+            'listings' => Listing::all()
+        ]);
+    }   
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Listing  $listing
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Listing  $listing
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Listing $listing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Listing  $listing
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Listing $listing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Listing  $listing
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Listing $listing)
+    {
+        //
+    }
+}
---
+++ resources/views/components/listing-tags.blade.php
@@ -0,0 +1,18 @@
+@props(['tagsCsv'])
+
+@php
+$tags = explode(', ', $tagsCsv);
+@endphp
+
+@unless(count($tags) == 0)
+
+<ul class="flex">
+    @foreach($tags as $tag)
+    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+        <a href="#">{{$tag}}</a>
+    </li>
+    @endforeach
+</ul>
+
+@endunless
+
